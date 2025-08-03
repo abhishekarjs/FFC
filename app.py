7bdeb9fc70f41d3ab33a159c8451c128b7d48055
abhishek <abhishekvaidhav@gmail.com>
2025-08-03 23:01:28 +0530

    Some modular changes to code

--- app.py
+++ app.py
@@ -19,20 +19,29 @@ def get_columns(ds):
         return columns
     except KeyError:
         print(f'SCHEMA nOT FOUND FOR {ds}')
-        return         
-    
+        return     
+        
+def process_file(src_base_dir,ds,tgt_base_dir):
+    for file in glob.glob(f'{src_base_dir}/{ds}/part*'):
+        df =pd.read_csv(file,names = get_columns(ds))
+        os.makedirs(f'{tgt_base_dir}/{ds}',exist_ok=True)
+        df.to_json(f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',orient = 'records',lines=True)
+        print(f'Num of records Processed for {os.path.split(file)[1]} in {ds} is {df.shape}')
+
+   
     
 def main():      
     src_base_dir = os.environ.get('SRC_BASE_DIR') 
     tgt_base_dir = os.environ.get('TGT_BASE_DIR')  
     for path in glob.glob(f'{src_base_dir}/*'):
         if os.path.isdir(path):
-            ds = os.path.split(path)[1]
-            for file in glob.glob(f'{path}/part*'):
-                df =pd.read_csv(file,names = get_columns(ds))
-                os.makedirs(f'{tgt_base_dir}/{ds}',exist_ok=True)
-                df.to_json(f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',orient = 'records',lines=True)
-                print(f'Num of records Processed for {os.path.split(file)[1]} in {ds} is {df.shape}')
+            process_file(src_base_dir,os.path.split(path)[1],tgt_base_dir)
+            # ds = os.path.split(path)[1]
+            # for file in glob.glob(f'{path}/part*'):
+            #     df =pd.read_csv(file,names = get_columns(ds))
+            #     os.makedirs(f'{tgt_base_dir}/{ds}',exist_ok=True)
+            #     df.to_json(f'{tgt_base_dir}/{ds}/part-{str(uuid.uuid1())}.json',orient = 'records',lines=True)
+            #     print(f'Num of records Processed for {os.path.split(file)[1]} in {ds} is {df.shape}')
 
 
 
