4313d230a971de7b4cfc0edb7a1ee81a42c29d87
abhishek <abhishekvaidhav@gmail.com>
2025-08-03 21:21:33 +0530

    Added core logic for FFM

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-def hw():
-    print("HELLO WORLD!!")
-    return 
+import json
+import uuid
+import os
+import glob
+import pandas as pd
 
-if __name__=="__main__":
-    hw()
+def get_columns(ds):
+    with open('data/retail_db/schemas.json') as fp:
+        schemas =   json.load(fp)
+        #print(schemas)
+    try:
+        schema = schemas.get(ds)
+        #print(schema)
+        if not schema:
+            raise KeyError
+        cols = sorted(schema , key = lambda s : s['column_position'])
+        columns = [col['column_name'] for col in cols]
+        return columns
+    except KeyError:
+        print(f'SCHEMA nOT FOUND FOR {ds}')
+        return         
+def main():        
+    for path in glob.glob('data/retail_db/*'):
+        if os.path.isdir(path):
+            ds = os.path.split(path)[1]
+            for file in glob.glob(f'{path}/part*'):
+                df =pd.read_csv(file,names = get_columns(ds))
+                os.makedirs(f'data/retail_demo/{ds}',exist_ok=True)
+                df.to_json(f'data/retail_demo/{ds}/part-{str(uuid.uuid1())}.json',orient = 'records',lines=True)
+                print(f'Num of records Processed for {os.path.split(file)[1]} in {ds} is {df.shape}')
+
+
+
+
+if __name__==  '__main__':
+    main()
